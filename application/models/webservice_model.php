2b025b165ad5cd90381f364c5ce8f511537053e3
vagrant <vagrant@scotchbox>
2020-04-26 10:34:42 +0000

    test with new model

--- application/models/webservice_model.php
+++ application/models/webservice_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class webservice_model extends CI_Model {
+class Webservice_model extends CI_Model {
 
     private $db = '';
 
