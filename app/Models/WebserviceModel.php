33b3823eb4966d1f5c1251c3e0294ed9d0616aa4
vagrant <vagrant@scotchbox>
2020-04-27 15:17:49 +0000

    migrating to codeignighter 4

---
+++ app/Models/WebserviceModel.php
@@ -0,0 +1,72 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class WebserviceModel extends Model
+{
+    protected $table = 'webservice2';
+    protected $allowedFields = array(
+        'key',
+        'value',
+        'created_at',
+        'updated_at',
+    );
+
+    public function getWebservice($slug = false)
+    {
+        if ($slug === false || empty($slug)) {
+            return $this->findAll();
+        }
+
+        return $this->asArray()
+                    ->where(['key' => $slug])
+                    ->first();
+    }
+
+    public function saveWebservice($data)
+    {
+        helper('date');
+        $prepared_data = array();
+        foreach ($data as $key => $value) {
+            $prepared_data = array(
+                'key' => $key,
+                'value' => $value,
+                'created_at' => now(),
+                'updated_at' => now(),
+            );
+            $this->insert($prepared_data);
+        }
+    }
+
+    // public function get_service($option =array())
+    // {
+    //     $sql = "SELECT `key`, `value`, UNIX_TIMESTAMP(created_at) AS created_at, UNIX_TIMESTAMP(updated_at) AS updated_at FROM webservice WHERE 1=1";
+
+    //     if (!empty($option['id']) && intval($option['id'])) {
+    //         $id = intval($option['id']);
+    //         $sql .= " AND `id` = '{$id}'";
+    //     }
+
+    //     if (!empty($option['key'])) {
+    //         $key = urldecode($option['key']); // when key had space in the 
+    //         $sql .= " AND `key` = '{$key}'";
+    //     }
+
+    //     if (!empty($option['timestamp'])) {
+    //         $sql .= "AND UNIX_TIMESTAMP(updated_at) = '{$option['timestamp']}'";
+    //     }
+
+    //     $query = $this->db->query($sql);
+
+    //     return (!empty($query->result())) ? $query->result() : array();
+    // }
+
+    // public function save_service($key, $value)
+    // {
+    //     $sql = "INSERT INTO webservice (`key`, `value`, created_at, updated_at) VALUES ('{$key}', '{$value}', NOW(), NOW()) ON DUPLICATE KEY UPDATE `value` = '{$value}', updated_at = NOW()";
+
+    //     $this->db->query($sql);
+
+    //     return $this->db->insert_id();
+    // }
+}
--- application/db-schema/dump.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `webservice` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `key` varchar(255) DEFAULT NULL,
-  `value` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `key_UNIQUE` (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
